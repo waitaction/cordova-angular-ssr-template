3f390f77f53f1e3ef3cd5b7ced83bc896b6d553f
QiJing Chen <qijing@wo.cn>
2020-10-08 14:23:04 +0800

    electron

--- hooks/compile-hook/build-desktop.js
+++ hooks/compile-hook/build-desktop.js
@@ -17,27 +17,27 @@ copyFile("src/index.html", "www/index.html");
 
 try {
     var cdvResult = child_process.execSync('cordova platform').toString();
-    if (/browser[\s\S]*Available/ig.test(cdvResult)) {
-        console.log('*** 已经安装cordova-ios ***');
+    if (/electron[\s\S]*Available/ig.test(cdvResult)) {
+        console.log('*** 已经安装electron ***');
     } else {
-        console.log('*** 正在安装cordova-ios ***');
-        child_process.execSync('cordova platform add ios', { stdio: [0, 1, 2] });
+        console.log('*** 正在安装electron ***');
+        child_process.execSync('cordova platform add electron', { stdio: [0, 1, 2] });
     }
 } catch (error) {
     console.log(error);
 }
 
 // 编译angular ng build
-console.log('*** 编译iOS ng run app:build:production  --output-path=www ***');
+console.log('*** 编译electron ng run app:build:production  --output-path=www ***');
 child_process.execSync('ng run app:build:production  --output-path=www', { stdio: [0, 1, 2] });
 
-// 编译cordova build ios --release --buildConfig
-console.log('*** 编译ios cordova build ios --release --buildConfig ***');
+// 编译cordova
+console.log('*** 编译electron cordova build electron --release --buildConfig ***');
 try {
-    child_process.execSync('cordova build ios --release --buildConfig --verbose', { stdio: [0, 1, 2] });
+    child_process.execSync('cordova build electron --release --buildConfig --verbose', { stdio: [0, 1, 2] });
     // 拷贝编译后的apk到dist文件夹
     let dist_folder = 'dist';
-    let dist_folder_ios = 'dist/ios';
+    let dist_folder_ios = 'dist/electron';
     if (!fs.existsSync(dist_folder)) {
         fs.mkdirSync(dist_folder);
     }
@@ -45,8 +45,7 @@ try {
         deleteFolder(dist_folder_ios);
     }
     fs.mkdirSync(dist_folder_ios);
-    copyFolder('platforms/ios/build/emulator/华辉人力.app', dist_folder_ios);
-
+    
 } catch (error) {
     console.log('');
     console.info("\033[31m *** 编译失败，请查看错误描述信息 *** \033[0m");
---
+++ hooks/compile-hook/debug-desktop.js
@@ -0,0 +1,40 @@
+/// 调试ios程序
+var fs = require('fs');
+const child_process = require('child_process');
+var deleteFolder = require('./shared/hook-fs').deleteFolder;
+var copyFile = require('./shared/hook-fs').copyFile;
+
+// 删除www文件 ， 创建www文件夹 ，拷贝index.html到www文件夹
+console.log('*** 清理缓存 ***');
+let www_folder = 'www';
+if (fs.existsSync(www_folder)) {
+    deleteFolder(www_folder);
+}
+fs.mkdirSync(www_folder);
+copyFile("src/index.html", "www/index.html");
+
+try {
+    var cdvResult = child_process.execSync('cordova platform').toString();
+    if (/electron[\s\S]*Available/ig.test(cdvResult)) {
+        console.log('*** 已经安装 electron ***');
+    } else {
+        console.log('*** 正在安装 electron ***');
+        child_process.execSync('cordova platform add electron', { stdio: [0, 1, 2] });
+    }
+} catch (error) {
+    console.log(error);
+}
+
+// 编译angular ng build
+console.log('*** 执行编译 ng build ***');
+child_process.execSync('ng run app:build --output-path=www', { stdio: [0, 1, 2] });
+
+// 编译cordova
+console.log('*** 启动调试 cordova run electron --emulator ***');
+try {
+    child_process.execSync('cordova run electron --emulator --verbose', { stdio: [0, 1, 2] });
+} catch (error) {
+    console.log('');
+    console.info("\033[31m *** 运行失败，请查看错误描述信息 *** \033[0m");
+    console.info("\033[31m *** 请确认你的安卓手机通过USB连接电脑，并已开启调试模式 *** \033[0m");
+}
